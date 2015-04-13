1fbe82ad70b4f6237a6ffe251514163f45fe83b7
tunime <almaseb_123@hotmail.com>
2015-04-13 10:37:30 -0500

    canbio
    de seguridad

--- js/misfun.js
+++ js/misfun.js
@@ -4,8 +4,13 @@ $('#segundo').fadeOut(0);
 $('#baner-hd').fadeIn(1000);
 
 });
+function nada (uso) {
+	uso=0;
+}
 
+var  uso=0;
 $('#uso').click(function() {
+uso++;
 $('#segundo').fadeOut(0);
 $("#segundo-s").fadeOut(0);
 $("#tercero-s").fadeOut(0);
@@ -14,38 +19,69 @@ $('#baner-hd').fadeOut(0);
 $('#segundo').fadeIn(1000);
  var inicio = 0;
 
+if (uso==1) {
 
-for (var inicio = 0; inicio < 4; inicio++) {
+	if (inicio==0) {
+	$("#primero-s").fadeIn(1000);
+	$("#primero-s").delay(4000).fadeOut(0);
+	console.log('soy el cero');
+	inicio++;
+	if (inicio==1) {
+		$("#segundo-s").delay(4000).fadeIn(1000);
+		$("#segundo-s").delay(3000).fadeOut(0);
+		console.log('soy el primero');
+		inicio++;
+			if (inicio==2) {
+				$("#tercero-s").delay(8000).fadeIn(1000);
+				$("#tercero-s").delay(3000).fadeOut(0);
+				console.log('soy el segundo');
+				setTimeout ('nada (uso)', 5000);
+				inicio++;
 
+			};
+	
 
-	switch(inicio){
-		case 0:
-			$("#primero-s").fadeIn(1000);
-			$("#primero-s").delay(4000).fadeOut(0);
-			console.log('soy el cero');
-		break;
-		case 1:
-			$("#segundo-s").delay(4000).fadeIn(1000);
-			$("#segundo-s").delay(3000).fadeOut(0);
-			console.log('soy el primero');
-		break;
-		case 2:
-			$("#tercero-s").delay(8000).fadeIn(1000);
-			$("#tercero-s").delay(3000).fadeOut(0);
-			console.log('soy el segundo');
-		break;
-		case 3:
-			$("#cuarto-s").delay(12000).fadeIn(1000);
-			$("#cuarto-s").delay(5000).fadeOut(0);
-			console.log('soy el tercero');
-
-		break;
-
-
-	}
+	};	
 
 
 };
+
+
+};
+
+
+
+// for (var inicio = 0; inicio < 4; inicio++) {
+
+
+// 	switch(inicio){
+// 		case 0:
+// 			$("#primero-s").fadeIn(1000);
+// 			$("#primero-s").delay(4000).fadeOut(0);
+// 			console.log('soy el cero');
+// 		break;
+// 		case 1:
+// 			$("#segundo-s").delay(4000).fadeIn(1000);
+// 			$("#segundo-s").delay(3000).fadeOut(0);
+// 			console.log('soy el primero');
+// 		break;
+// 		case 2:
+// 			$("#tercero-s").delay(8000).fadeIn(1000);
+// 			$("#tercero-s").delay(3000).fadeOut(0);
+// 			console.log('soy el segundo');
+// 		break;
+// 		case 3:
+// 			$("#cuarto-s").delay(12000).fadeIn(1000);
+// 			$("#cuarto-s").delay(5000).fadeOut(0);
+// 			console.log('soy el tercero');
+
+// 		break;
+
+
+// 	}
+
+
+// };
  // $("4pri++ro-s").delay(300).fadeIn(000);
 });
 // culminado el uso
